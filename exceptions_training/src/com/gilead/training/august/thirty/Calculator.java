475609fa4d0cfad06f5c345f595f17de373563e9
HP <rajapandian84@gmail.com>
2023-08-31 10:18:00 +0530

    Exception

---
+++ exceptions_training/src/com/gilead/training/august/thirty/Calculator.java
@@ -0,0 +1,5 @@
+package com.gilead.training.august.thirty;
+
+public interface Calculator {
+    public  float divide(float dividend,float divisor);
+}
---
+++ exceptions_training/src/com/gilead/training/august/thirty/CalculatorImpl.java
@@ -0,0 +1,8 @@
+package com.gilead.training.august.thirty;
+
+public class CalculatorImpl implements Calculator{
+    @Override
+    public float divide(float dividend, float divisor) {
+        return dividend/divisor;
+    }
+}

31508453369f14e54c6e1aa07afcb0debc8209db
HP <rajapandian84@gmail.com>
2023-09-01 09:58:05 +0530

    Add

---
+++ looptrainingjava/src/com/gilead/sep/one/EnhancedForLoopRepeaterInt.java
@@ -0,0 +1,11 @@
+package com.gilead.sep.one;
+
+public class EnhancedForLoopRepeater {
+    public void printTimes() {
+        int[] intArr = {1, 2, 3, 4, 5};
+        //enhanced for loop
+        for (int num : intArr) {
+            System.out.print(num + "  ");
+        }
+    }
+}
---
+++ looptrainingjava/src/com/gilead/sep/one/EnhancedForLoopRepeaterString.java
@@ -0,0 +1,2 @@
+package com.gilead.sep.one;public class EnhancedForLoopRepeaterString {
+}

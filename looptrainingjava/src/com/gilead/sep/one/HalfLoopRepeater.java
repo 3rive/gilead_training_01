4315ea41eee85ac274889b2694491cece5c02514
HP <rajapandian84@gmail.com>
2023-09-01 10:02:50 +0530

    Add

---
+++ looptrainingjava/src/com/gilead/sep/one/HalfLoopRepeater.java
@@ -0,0 +1,9 @@
+package com.gilead.sep.one;
+
+public class HalfLoopRepeater {
+    public void printHalfTimes(int times){
+        for (int i = 1; i <= times; i++) {
+            System.out.print(i + "  ");
+        }
+    }
+}

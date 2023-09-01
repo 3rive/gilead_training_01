349eed79afa7758340a3cf8fe071bfe8786bd3e7
HP <rajapandian84@gmail.com>
2023-09-01 08:40:07 +0530

    Add

--- looptrainingjava/src/com/gilead/sep/one/LoopDemo.java
+++ looptrainingjava/src/com/gilead/sep/one/LoopDemo.java
@@ -2,10 +2,7 @@ package com.gilead.sep.one;
 
 public class LoopDemo {
     public static void main(String[] args) {
-        //print numbers for 1 to 20
-        //for loop
-        for (int i = 1; i<=20; i++){
-          System.out.print(i+"  ");
-        }
+        Repeater r = new Repeater();
+        r.printTimes(5);
     }
 }
---
+++ looptrainingjava/src/com/gilead/sep/one/Repeater.java
@@ -0,0 +1,10 @@
+package com.gilead.sep.one;
+
+public class Repeater {
+    public void printTimes(int times){
+        for ( int i = 1; i<=times; i++){
+            System.out.print(i+"  ");
+        }
+
+    }
+}

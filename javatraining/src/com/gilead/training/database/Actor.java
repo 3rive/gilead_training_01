3c3764958713443e59f6d57b67fb98345c810ee7
HP <rajapandian84@gmail.com>
2023-09-11 09:54:47 +0530

    Committed code

---
+++ javatraining/src/com/gilead/training/database/Actor.java
@@ -0,0 +1,46 @@
+package com.gilead.training.database;
+
+import java.sql.Timestamp;
+
+public class Actor {
+    private int actorId;
+
+    public int getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(int actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    private String firstName;
+    private String lastName;
+    private Timestamp lastUpdate;
+
+
+
+}
--- javatraining/src/com/gilead/training/database/MySQLDatabaseConnectorDemo.java
+++ javatraining/src/com/gilead/training/database/MySQLDatabaseConnectorDemo.java
@@ -1,37 +1,50 @@
 package com.gilead.training.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MySQLDatabaseConnectorDemo {
     public static void main(String[] args) {
         Connection connection = null;
-        try{
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila","root","Letmein@123");
-            if(connection !=null){
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root", "Letmein@123");
+            if (connection != null) {
                 Statement statement;
                 statement = connection.createStatement();
                 ResultSet resultSet;
                 resultSet = statement.executeQuery("select *from actor order by first_name asc");
                 String actorFirstName;
                 String actorLastName;
-                if(resultSet !=null){
+                int actorId;
+                Timestamp lastUpdate;
+                List<Actor> actorsinDB = new ArrayList<Actor>();
+                if (resultSet != null) {
                     System.out.println("Database Connected and query executed!!!");
-                    while(resultSet.next()){
+                    while (resultSet.next()) {
+                        Actor actor = new Actor();
+                        actorId = resultSet.getInt("actor_id");
                         actorFirstName = resultSet.getString("first_name");
                         actorLastName = resultSet.getString("last_name");
-                        System.out.println("Name :::: First Name : "+ actorFirstName + "  Last Name : "+ actorLastName);
+                        lastUpdate = resultSet.getTimestamp("last_update");
+                        actor.setActorId(actorId);
+                        actor.setFirstName(actorFirstName);
+                        actor.setLastName(actorLastName);
+                        actor.setLastUpdate(lastUpdate);
+                        actorsinDB.add(actor);
                     }
                 }
-            }
-            else{
+                for (Actor actorFromDB : actorsinDB) {
+                    //print the actor's first name whose ID is 132
+                    if (actorFromDB.getActorId() == 132) {
+                        System.out.println("Full Name ::: " + actorFromDB.getFirstName() + " , " + actorFromDB.getLastName());
+                    }
+                }
+            } else {
                 System.out.println("Database Not Connected - Check your DB properties");
             }
-        }
-        catch(Exception exception){
+        } catch (Exception exception) {
             System.out.println(exception.toString());
         }
 
